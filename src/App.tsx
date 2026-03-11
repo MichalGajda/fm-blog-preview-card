731ec23ed76c4c9b329389863199bb5e42a6fcaa
Michal Gajda <mgajda36@gmail.com>
2026-03-11 22:46:16 +0100

    feat: added initial/draft structure for blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
-import Base from './components/Base'
+import Attribution from './components/Attribution'
 
 function App() {
 
   return (
     <>
-      <Base />  
+      <main>
+        <div className="blog-card">
+          <div className="blog-card__image"></div>
+          <div className="blog-card__tag"></div>
+          <p className="blog-card__date"></p>
+          <h1 className="blog-card__heading"></h1>
+          <p className="blog-card__description"></p>
+          <div className="blog-card__author">
+            <img src="/image-avatar.webp" alt="Picture of the author of the blog card" className="author__image" />
+            <p className="author__name"></p>
+          </div>
+        </div>
+      </main>
+      <Attribution />
     </>
   )
 }
